6d12798d5eea4e4d15ef18908cfdf6a358bc72d0
Abdelrahman Mohamed <mrabdelrahman10@gmail.com>
2020-01-31 05:04:13 +0300

    Update banner reponse

--- modules/banner/controller.js
+++ modules/banner/controller.js
@@ -13,14 +13,13 @@ module.exports.random_banner = function (req, res) {
 	req.custom.clean_filter.position = req.params.Id;
 	req.custom.cache_key = `${collectionName}_${req.custom.lang}_${req.custom.clean_filter.position}`;
 	mainController.list_all(req, res, collectionName, {
-		position: 1,
+		_id: 0,
 		bannertype: 1,
 		code: 1,
 		picture: 1,
 		url: 1,
 	}, (data) => {
-		const i = data.data[Math.floor(Math.random() * data.data.length)];
-		const out = i.bannertype == 1 ? i.code : `<a href="${i.url}"><img src="${i.picture}" style="width: 100%;" /></a>`;
+		const out = data.data[Math.floor(Math.random() * data.data.length)];
 		res.out(out);
 	});
 };
