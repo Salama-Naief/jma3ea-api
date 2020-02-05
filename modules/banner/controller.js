15183a8f1386ac9d0e381b553b0d7ac523d54f7c
Abdelrahman Mohamed <mrabdelrahman10@gmail.com>
2020-02-05 04:58:35 +0300

    Show banners by category

--- modules/banner/controller.js
+++ modules/banner/controller.js
@@ -2,6 +2,7 @@
 
 // Load required modules
 const mainController = require("../../libraries/mainController");
+const ObjectID = require('mongodb').ObjectID;
 const collectionName = 'banner';
 
 /**
@@ -12,6 +13,9 @@ const collectionName = 'banner';
 module.exports.random_banner = function (req, res) {
 	req.custom.clean_filter.position = req.params.Id;
 	req.custom.cache_key = `${collectionName}_${req.custom.lang}_${req.custom.clean_filter.position}`;
+	if (req.query.category && ObjectID.isValid(req.query.category)) {
+		req.custom.clean_filter.categories = req.query.category;
+	}
 	mainController.list_all(req, res, collectionName, {
 		_id: 0,
 		bannertype: 1,
