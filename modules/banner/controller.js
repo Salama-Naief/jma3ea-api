53cfb6e5924593e63525ce3253a55262004d002f
Abdelrahman Mohamed <mrabdelrahman10@gmail.com>
2020-02-05 20:15:57 +0300

    Fix banners in categories

--- modules/banner/controller.js
+++ modules/banner/controller.js
@@ -14,7 +14,25 @@ module.exports.random_banner = function (req, res) {
 	req.custom.clean_filter.position = req.params.Id;
 	req.custom.cache_key = `${collectionName}_${req.custom.lang}_${req.custom.clean_filter.position}`;
 	if (req.query.category && ObjectID.isValid(req.query.category)) {
-		req.custom.clean_filter.categories = req.query.category;
+		req.custom.clean_filter['$or'] = [{
+				categories: {
+					$exists: false
+				}
+			},
+			{
+				categories: {
+					$eq: []
+				}
+			},
+			{
+				categories: {
+					$eq: null
+				}
+			},
+			{
+				categories: req.query.category
+			}
+		];
 	}
 	mainController.list_all(req, res, collectionName, {
 		_id: 0,
