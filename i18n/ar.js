9e68b2ccd858dc42383ca079d4e7fbec9554b8a6
Abdelrahman Mohamed <mrabdelrahman10@gmail.com>
2021-02-07 10:05:49 +0300

    Update message

--- i18n/ar.js
+++ i18n/ar.js
@@ -115,7 +115,7 @@ const locals = {
 		value,
 		currency
 	}) => {
-		return `يجب ان يكون الطلب اكثر من ${value} ${currency}`
+		return `يجب أن تكون قيمة المشتريات ${value} ${currency}`
 	},
 	fields: {
 		address_id: 'العنوان',
