6068495971fd759b028f2d165d3867db5904d14c
Abdelrahman Mohamed <mrabdelrahman10@gmail.com>
2021-11-28 21:19:21 +0300

    Fix

--- libraries/middlewares/update_language.js
+++ libraries/middlewares/update_language.js
@@ -4,13 +4,13 @@ const ObjectID = require('../../types/object_id');
  * Middleware for sorting
  */
 module.exports = (req, res, next) => {
-	if (req.custom.authorizationObject && req.custom.lang != req.custom.authorizationObject.language) {
+	if (req.custom.authorizationObject && req.custom.lang != req.custom.authorizationObject.language && req.custom.authorizationObject._id) {
 		const collection = req.custom.db.client().collection('member');
 		const data = {
 			language: req.custom.lang
 		};
 		collection.updateOne({
-			_id: ObjectID(req.custom.authorizationObject.member_id.toString()),
+			_id: ObjectID(req.custom.authorizationObject._id.toString()),
 		}, {
 			$set: data
 		}).then(() => {
