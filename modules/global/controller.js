05829298e0d81383caa89df6eeb0632891e9c985
IAmJakPro <jakdev20@gmail.com>
2022-06-17 21:41:25 +0100

    Fixed status issue

--- modules/global/controller.js
+++ modules/global/controller.js
@@ -1,3 +1,5 @@
+const { ObjectId } = require("mongodb");
+
 module.exports.cleanProductsQuantities = async function (req, res) {
     try {
         const collection = req.custom.db.client().collection("product");
@@ -53,17 +55,17 @@ module.exports.cleanProductsStatuses = async function (req, res) {
         const collection = req.custom.db.client().collection("product");
 
         const results = await collection.update({
-            variants: { $exists: true },
+            prod_n_storeArr: { $exists: true },
             $expr: {
                 $lt: [
                     {
                         $reduce: {
-                            input: "$variants",
+                            input: "$prod_n_storeArr",
                             initialValue: 0,
                             in: {
                                 "$add": [
                                     "$$value",
-                                    { $sum: "$$this.prod_n_storeArr.quantity" }
+                                    "$$this.quantity"
                                 ]
                             }
                         }
@@ -73,7 +75,7 @@ module.exports.cleanProductsStatuses = async function (req, res) {
             },
         }, [
             {
-                $set: { "prod_n_storeArr.status": false }
+                $set: { "status": false }
             }
         ], { multi: true });
 
