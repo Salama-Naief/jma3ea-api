1f6624a7757f94ce034f228dbc2283e464070dca
IAmJakPro <jakdev20@gmail.com>
2022-07-06 20:09:53 +0100

    Add products filtering and improve brands

--- modules/brand/controller.js
+++ modules/brand/controller.js
@@ -10,7 +10,11 @@ const collectionName = 'brand';
  * @param {Object} res
  */
 module.exports.list = function (req, res) {
-	req.custom.cache_key = `${collectionName}_${req.custom.lang}_page_${req.custom.skip}`;
+	if (req.query && req.query['starts_with'] && req.query['starts_with'] !== "all") {
+		req.custom.clean_filter['name.' + req.custom.lang] = { $regex: new RegExp("^" + req.query['starts_with'].toLowerCase(), "i") }
+	} else {
+		req.custom.cache_key = `${collectionName}_${req.custom.lang}_page_${req.custom.skip}`;
+	}
 	mainController.list(req, res, collectionName, {
 		"_id": 1,
 		"name": {
@@ -28,7 +32,7 @@ module.exports.list = function (req, res) {
  * @param {Object} req
  * @param {Object} res
  */
-module.exports.read = function (req, res) {
+ module.exports.read = function (req, res) {
 	mainController.read(req, res, collectionName, {
 		"_id": 1,
 		"name": {
@@ -38,5 +42,36 @@ module.exports.read = function (req, res) {
 			$ifNull: [`$description.${req.custom.lang}`, `$description.${req.custom.config.local}`]
 		},
 		"picture": 1
+	}, async (data) => {
+		const collection = req.custom.db.client().collection("category");
+		const categories = await collection.aggregate([
+			{ $match: { status: true } },
+			{
+				$lookup:
+				{
+					from: "product",
+					localField: "_id",
+					foreignField: "prod_n_categoryArr.category_id",
+					pipeline: [
+						{
+							$match: { $and: [{ brand_id: new ObjectId(data._id) }, { status: true }] },
+						}
+					],
+					as: "products"
+				}
+			},
+			{ $match: { $expr: { $gt: [{ $size: "$products" }, 0] } } },
+			{
+				$project: {
+					"_id": 1,
+					"name": {
+						$ifNull: [`$name.${req.custom.lang}`, `$name.${req.custom.config.local}`]
+					},
+					"products_count": { $size: "$products" }
+				}
+			}
+		]).toArray();
+		if (categories) data.categories = categories;
+		return res.out(data);
 	});
 };
