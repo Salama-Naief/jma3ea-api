99f65b9d951c27da4eef084ba254b8b9febb704a
Yusuf Yurtsever <yusuf.yurtsever@zarcony.com>
2022-07-16 01:19:12 +0300

    adds debug mail

--- libraries/mail.js
+++ libraries/mail.js
@@ -2,18 +2,18 @@ const config = require('../config');
 const nodemailer = require('nodemailer');
 
 module.exports.send_mail = async function (from, from_name, to, subject, body) {
-	const valid_sender = from.host && from.port && from.secure && from.username && from.password;
+	// const valid_sender = from.host && from.port && from.secure && from.username && from.password;
+	// console.log(valid_sender);
+	const host = config.mail.host;
+	const port = config.mail.port;
+	const secure = config.mail.secure;
+	const username = config.mail.username;
+	const password = config.mail.password;
 
-	const host = valid_sender ? from.host : config.mail.host;
-	const port = valid_sender ? from.port : config.mail.port;
-	const secure = valid_sender ? from.secure : config.mail.secure;
-	const username = valid_sender ? from.username : config.mail.username;
-	const password = valid_sender ? from.password : config.mail.password;
-	// create reusable transporter object using the default SMTP transport
 	const transporter = nodemailer.createTransport({
 		host: host,
 		port: port,
-		secure: secure, // true for 465, false for other ports
+		secure: true, // true for 465, false for other ports
 		auth: {
 			user: username, // generated ethereal user
 			pass: password // generated ethereal password
@@ -22,7 +22,6 @@ module.exports.send_mail = async function (from, from_name, to, subject, body) {
 			rejectUnauthorized: false
 		}
 	});
-
 	// setup email data with unicode symbols
 	const mailOptions = {
 		from: `"${from_name} " <${username}>`, // sender address
@@ -30,10 +29,12 @@ module.exports.send_mail = async function (from, from_name, to, subject, body) {
 		subject: `${from_name} : ${subject}`, // Subject line
 		html: body // html body
 	};
-	var result = await transporter.sendMail(mailOptions);
-	console.log(result,mailOptions);
-	return result;
-
-	// send mail with defined transport object
-	return transporter.sendMail(mailOptions)
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+		}else{
+			console.log(info);
+			resolve( info);
+		}
+	});
 };
