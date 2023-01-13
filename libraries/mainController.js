cacc0d8345d3c0f63d4fb084b16ca3314241f701
IAmJakPro <jakdev20@gmail.com>
2023-01-13 15:27:35 +0100

    fixing discount expiry date

--- libraries/mainController.js
+++ libraries/mainController.js
@@ -168,7 +168,7 @@ module.exports.list = function (req, res, collectionName, projection, callback) 
 						}, status_message.UNEXPECTED_ERROR);
 					}
 
-					results = results ? results.map((i) => {
+					results = results ? results.map(async (i) => {
 						if (i.picture) {
 							i.picture = `${req.custom.config.media_url}${i.picture}`;
 						}
@@ -177,13 +177,7 @@ module.exports.list = function (req, res, collectionName, projection, callback) 
 							if (p.old_price && p.discount_price_valid_until && p.discount_price_valid_until < new Date()) {
 								const oldPrice = parseFloat(p.old_price);
 								p.price = oldPrice;
-								resetPrice(req, p.sku, oldPrice).then(isPriceResetted => {
-									if (!isPriceResetted) {
-										res.out({
-											message: req.custom.local.unexpected_error
-										}, status_message.UNEXPECTED_ERROR);
-									}
-								});
+								await resetPrice(req, p.sku, oldPrice);
 							}
 
 							i.price = common.getFixedPrice(i.price);
