ddb84a334882def34ab0cbf1341969526956e6f0
IAmJakPro <jakdev20@gmail.com>
2023-04-06 12:20:09 +0000

    fix listing upplier reviews

--- modules/review/controller.js
+++ modules/review/controller.js
@@ -75,12 +75,14 @@ module.exports.list = (req, res) => {
 
     req.custom.clean_filter['supplier_id'] = ObjectID(req.query.supplier_id);
 
+    console.log('REVIEW SUPPLIER FILTER: ', req.custom.clean_filter);
+
     mainController.list(req, res, COLLECTION_NAME, {
         "name": 1,
         "stars": 1,
         "comment": 1,
         "member_id": 1
-    }, async (out) => {
+    }/* , async (out) => {
         if (out.data.length === 0) {
             return res.out(out, status_message.NO_DATA);
         }
@@ -102,6 +104,6 @@ module.exports.list = (req, res) => {
 
         res.out(out);
 
-    });
+    } */);
 
 }
