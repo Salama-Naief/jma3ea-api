32db3ed2e2b4089a13fcd5f751c5da3382a048bf
IAmJakPro <jakdev20@gmail.com>
2023-04-10 14:28:13 +0000

    fix search

--- libraries/common.js
+++ libraries/common.js
@@ -100,7 +100,7 @@ module.exports.filter_internal_suppliers_by_city = async function (req) {
 
 			if (req.custom.clean_filter.hasOwnProperty('supplier_id')) {
 				return req.custom.clean_filter;
-			} */
+			}
 
 			if (req.custom.clean_filter.hasOwnProperty('$or')) {
 				if (req.custom.clean_filter.hasOwnProperty('$and')) {
