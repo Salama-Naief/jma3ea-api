3ef68bf62dcdc06d8c0253467084c13e8fa666b0
IAmJakPro <jakdev20@gmail.com>
2023-07-22 00:01:10 +0100

    add elastic search

--- modules/global/controller.js
+++ modules/global/controller.js
@@ -234,7 +234,7 @@ module.exports.indexProducts = async (req, res) => {
         }
 
         console.log('=============================================== Indexing completed. =====================================================');
-        return res.out({ message: 'Indexing completed', count: products.length });
+        return res.out({ message: 'Indexing completed', count: products.length, skip: skip });
     } catch (error) {
         console.error('Error indexing products:', error);
         return res.out(error);
