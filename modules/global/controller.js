f393300d30c6a05aabd680a0fdadad3573438cc3
IAmJakPro <jakdev20@gmail.com>
2023-07-23 13:55:18 +0100

    get all elastic search indexes

--- modules/global/controller.js
+++ modules/global/controller.js
@@ -235,6 +235,9 @@ module.exports.indexProducts = async (req, res) => {
                 },
             };
 
+            if (product._id.toString() === '5d4bddc645de0376bb50e8e6')
+                console.log('=============================================== UPDATE PRODUCT \n', updateBody.doc, '===============================================');
+
             await esClient.update({
                 index: 'products',
                 id: product._id.toString(),
