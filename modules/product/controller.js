10f91936eaac80186dc3c49699ea6d262457e3e8
IAmJakPro <jakdev20@gmail.com>
2023-07-27 15:21:59 +0100

    fx expiratio ndate of featrue categories

--- modules/product/controller.js
+++ modules/product/controller.js
@@ -343,7 +343,7 @@ module.exports.featured = async function (req, res) {
 	}
 
 
-	if (cache_key) {
+	if (false) {
 		let cached_data = await cache.get(cache_key).catch(() => null);
 		if (cached_data) {
 			cached_data = cached_data.map((feature_category) => {
