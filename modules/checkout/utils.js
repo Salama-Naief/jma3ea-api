87686fa62976d4caf3149c7db8f5705d8a9f2bbb
unknown <jakdev20@gmail.com>
2023-09-07 18:24:48 +0200

    fixing oicking times and improve offers

--- modules/checkout/utils.js
+++ modules/checkout/utils.js
@@ -203,7 +203,7 @@ module.exports.getDeliveryTimes = async (req, cityObj, supplier = {}) => {
             const full_date = today.add(idx, 'hours').format(req.custom.config.date.format);
             const time = supplier.has_picking_time ? today.format('LT') : today.format('LT') + ' : ' + today.add(2, 'hours').format('LT');
             
-            if (supplier.has_picking_time) console.log("TIMES DEBUG: ", today.format('LT'), today.format('LT') + ' : ' + today.add(2, 'hours').format('LT'))
+            if (supplier.has_picking_time) console.log("TIMES DEBUG: ", time)
 
             const cache_key_dt = `delivery_times_${supplier.is_external ? supplier._id.toString() : ''}_${day}_${idx}`;
             const cached_delivery_times = parseInt(await cache.get(cache_key_dt).catch(() => null) || 0);
