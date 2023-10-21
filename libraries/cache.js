8075ee37287aa3aaf3805f9fcbd7b4e34e770131
unknown <jakdev20@gmail.com>
2023-10-21 20:05:22 +0200

    fx

--- libraries/cache.js
+++ libraries/cache.js
@@ -10,18 +10,15 @@ const client = redis.createClient({
 		if (options.error && options.error.code === "ECONNREFUSED") {
 			// End reconnecting on a specific error and flush all commands with
 			// a individual error
-			console.log('#1');
 			return new Error("The server refused the connection");
 		}
 		if (options.total_retry_time > 1000 * 60 * 60) {
 			// End reconnecting after a specific timeout and flush all commands
 			// with a individual error
-			console.log('#2');
 			return new Error("Retry time exhausted");
 		}
 		if (options.attempt > 10) {
 			// End reconnecting with built in error
-			console.log('#3');
 			return undefined;
 		}
 		// reconnect after
